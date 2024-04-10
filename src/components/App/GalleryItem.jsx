b81805415cb46b40500cb0770b0d1bbc5dd218e2
Erik Silcox <eriksilcox651@gmail.com>
2024-04-09 21:38:29 -0500

    created getGalleryList function GET route

--- src/components/App/GalleryItem.jsx
+++ src/components/App/GalleryItem.jsx
@@ -1,4 +1,16 @@
-function galleryItem() {
+function galleryItem({ gallery }) {
+
+  const [toggle, setToggle] = useState(true)
+
+  const handleClick = () => {
+
+  };
+  return ( 
+  <li onClick={handleClick} data-testid="galleryItem">
+    {toggle ? gallery.image : gallery.text}
+  </li> 
+    
+  )
 
 }
 
--- src/components/App/GalleryList.jsx
+++ src/components/App/GalleryList.jsx
@@ -1,12 +1,29 @@
 function galleryList({galleryList}) {
 
+  let [galleryList, setGalleryList] = useState([]);
+
+  useEffect(() => {
+    getGalleryList();
+  }, []);
+
+  const getGalleryList = () => {
+    axios.get('/api/gallery')
+    .then((response) => {
+      setGalleryList(response.data);
+    })
+    .catch((error) => {
+      alert('error getting gallery list');
+      console.log(error)
+    });
+  };
+
   return (
     <><div>
       <ul data-testid="galleryList">
         <h2>Gallery List</h2>
         <ul>
-        {galleryList.map((month) => (
-          <GalleryItem key={galleryList.id}
+        {galleryList.map((galleyImage) => (
+          <GalleryItem key={galleryImage.id}
           gallery={gallery} />
         ))};
 </ul>
